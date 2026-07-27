d788e1cff3f432e4c2c509ed79bf69cb8fc5d247
James Prevett <jp@jamespre.dev>
2026-07-27 11:46:42 -0500

    Added doc comments

--- src/node/rl.ts
+++ src/node/rl.ts
@@ -11,11 +11,17 @@ _rl[Symbol.dispose] = () => process.stdin.unref?.();
 
 process.stdin.unref?.();
 
+/**
+ * Get a readline reference. While held, this means process.stdin is ref'ed if it's a TTY.
+ */
 export function getReadline(): Interface {
 	process.stdin.ref?.();
 	return _rl;
 }
 
+/**
+ * Confirm a yes/no question
+ */
 export async function confirm(question?: string | null, defaultAnswer: boolean = false): Promise<boolean> {
 	using rl = getReadline();
 	const raw = await rl.question(`${question || 'Is this ok'} [${defaultAnswer ? 'Y/n' : 'y/N'}]: `).catch(() => null);
@@ -27,6 +33,9 @@ export async function confirm(question?: string | null, defaultAnswer: boolean =
 	return !!error || data;
 }
 
+/**
+ * Assert that the answer to a question is yes, otherwise fail and exit
+ */
 export async function assertYes(question?: string | null, failureMessage: string = 'Aborted.'): Promise<void> {
 	const yes = await confirm(question);
 	if (!yes) exit(failureMessage);
